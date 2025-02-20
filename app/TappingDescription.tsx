c7ffede86097f8f273f9d9ffa7947c0b7d9478b2
katrinesundsbak <katrine.sundsbak@hotmail.com>
2025-02-20 12:15:55 +0100

    target

--- app/TappingDescription.tsx
+++ app/TappingDescription.tsx
@@ -1,6 +1,7 @@
 import Description from "@/components/reusable/Description"
 import Headline from "@/components/reusable/Headline"
 import PainButton from "@/components/reusable/PainButton"
+import Target from "@/components/reusable/Target"
 import { View } from "react-native"
 
 const TappingDescription = () => {
@@ -15,6 +16,7 @@ const TappingDescription = () => {
         <Headline text={"Tapping-Task"}/>
         <Description text={"A circle appears on the screen and you have to press it as quickly and accurately as possible"} />
         <PainButton href={"/TappingTasks"} text={"Continue"} />
+        <Target/>
     </View>
   )
 }
