6f91ca1dbfb15d247fcb49759abc1fec4b24310c
sashanasivasothy <127312457+sashanasivasothy@users.noreply.github.com>
2025-02-20 14:33:52 +0100

    FindIcon ændringer

--- app/xFindIconTaskOne.tsx
+++ app/xFindIconTaskOne.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View, Text } from "react-native";
 import Headline from '@/components/reusable/Headline'
+import Description from '@/components/reusable/Description';
+import PainButton from '@/components/reusable/PainButton';
 
 //Side 1 af FIND_ICON
 
@@ -11,8 +13,28 @@ export default function xFindIconTaskOne() {
         flex: 1, alignItems: 'center', 
     }}
   >
-      <Headline text={"Task 1"}/>
-    <Text style={{ fontSize: 20 }}>kk</Text>
+      <Headline text={"Find Icon"}/>
+    <Text style={{ fontSize: 20 }}>Find the icon on the display</Text>
+    <View
+    style={{
+      backgroundColor: '#FEFEFE',
+      justifyContent: "center",
+      width: 200,
+      height: 200,
+      borderRadius: 10,
+      marginTop: 20,
+      marginBottom: 10,
+      alignItems: 'center', 
+      
+    }}
+    >
+    <Description text={"Icon"}/>
+
+    </View>
+
+    <Description text={"Icon name"}/>
+    <PainButton href={"/xFindIconTaskTwo"} text={"continue"}/>
+    
   </View>
   )
 }
--- app/xFindIconTaskTwo.tsx
+++ app/xFindIconTaskTwo.tsx
@@ -1,18 +1,82 @@
 import React from 'react'
-import { View, Text } from "react-native";
-import Headline from '@/components/reusable/Headline'
+import { View } from 'react-native'
+import Description from '@/components/reusable/Description'
 
-//side 2 af FIND_ICON
+const xFindIconTaskTwo = () => {
+const rowHeight = 100/6
 
-export default function xFindIconTask2() {
+  const columnStyle = {
+    flexDirection: "column",
+    width: "100%",
+    height: "100%"
+  }
+  const rowStyle = {
+    flexDirection: "row",
+    height: `${rowHeight}%`
+  }
+  const boxStyle = {
+    borderWidth: 1,
+    width: "25%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+
+  // tilføje icons
   return (
-    <View
-    style={{
-        flex: 1, alignItems: 'center', 
-    }}
-  >
-      <Headline text={"Task 2"}/>
-    <Text style={{ fontSize: 20 }}>task 2</Text>
-  </View>
+    <View style={columnStyle}>
+      <View style={rowStyle}>
+        <View style={boxStyle} >1
+          <View
+          style={{
+            height: 80,
+            width: 80,
+            borderRadius: 10,
+            backgroundColor: "#000000",
+            marginTop: 20
+          }}
+          >
+          </View>
+          <Description text={"Icon"}/> 
+        </View>
+
+        <View style={boxStyle} >2</View>
+        <View style={boxStyle} >3</View>
+        <View style={boxStyle} >4</View>
+      </View>
+      <View style={rowStyle}>
+        <View style={boxStyle} >5</View>
+        <View style={boxStyle} >6</View>
+        <View style={boxStyle} >7</View>
+        <View style={boxStyle} >8</View>
+      </View>
+      <View style={rowStyle}>
+        <View style={boxStyle} >9</View>
+        <View style={boxStyle} >10</View>
+        <View style={boxStyle} >11</View>
+        <View style={boxStyle} >12</View>
+      </View>
+      <View style={rowStyle}>
+        <View style={boxStyle} >13</View>
+        <View style={boxStyle} >14</View>
+        <View style={boxStyle} >15</View>
+        <View style={boxStyle} >16</View>
+      </View>
+      <View style={rowStyle}>
+        <View style={boxStyle} >17</View>
+        <View style={boxStyle} >18</View>
+        <View style={boxStyle} >19</View>
+        <View style={boxStyle} >20</View>
+      </View>
+      <View style={rowStyle}>
+        <View style={boxStyle} >21</View>
+        <View style={boxStyle} >22</View>
+        <View style={boxStyle} >23</View>
+        <View style={boxStyle} >24</View>
+      </View>
+    </View>
   )
 }
+
+export default xFindIconTaskTwo
