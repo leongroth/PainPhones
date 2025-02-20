76b8b8205d5e09a1d0a06b543d4a44e77f3a3744
sashanasivasothy <127312457+sashanasivasothy@users.noreply.github.com>
2025-02-20 12:15:39 +0100

    FindIcon changes

--- app/xFindIconDescription.tsx
+++ app/xFindIconDescription.tsx
@@ -2,16 +2,20 @@ import React from 'react'
 import { View, Text } from "react-native";
 import Headline from '@/components/reusable/Headline'
 import PainButton from '@/components/reusable/PainButton'
+import { xFindIconTask1 } from './xFindIconTask1';
+
+//Startside af FIND_ICON 
 
 export const xFindIconDescription = () => {
   return (
     <View
     style={{
-        flex: 1, alignItems: 'center'
+        flex: 1, alignItems: 'center', 
     }}
   >
       <Headline text={"Find Icon"}/>
     <Text style={{ fontSize: 20 }}>A icon is shown to you and you have to find it with other icons in the mix </Text>
+    <PainButton href={'/xFindIconTask1'} text={"continue"}/>
   </View>
   )
 }
--- app/xFindIconTask2.tsx
+++ app/xFindIconTask2.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { View, Text } from "react-native";
+import Headline from '@/components/reusable/Headline'
+
+//side 2 af FIND_ICON
 
 export const xFindIconTask2 = () => {
   return (
-    <div>xFindIconTask2</div>
+    <View
+    style={{
+        flex: 1, alignItems: 'center', 
+    }}
+  >
+      <Headline text={"Task 2"}/>
+    <Text style={{ fontSize: 20 }}>task 2</Text>
+  </View>
   )
 }
