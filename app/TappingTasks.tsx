a377255dc7c44c06eaa2763fed6d46cb15b07352
katrinesundsbak <katrine.sundsbak@hotmail.com>
2025-02-20 11:44:36 +0100

    push

--- app/TappingTasks.tsx
+++ app/TappingTasks.tsx
@@ -1,8 +1,103 @@
+import { View } from "react-native"
+
 const TappingTasks = () => {
   return (
-    <div>
-      
-    </div>
+    <View
+      style={{
+        flex: "4",
+        width: "100%",
+        height: "100%"
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          backgroundColor: "blue",
+          height: "16,5%",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "red",
+            borderStyle: "solid",
+            borderWidth: 3,
+            width: "25%",
+            height: "16,5%"
+          }}
+          ></View>
+          <View
+          style={{
+            backgroundColor: "yellow",
+            borderStyle: "solid",
+            borderWidth: 3,
+            width: "25%",
+            height: "16,5%"
+          }}
+          ></View>
+                  <View
+          style={{
+            backgroundColor: "red",
+            borderStyle: "solid",
+            borderWidth: 3,
+            width: "25%",
+            height: "16,5%"
+          }}
+          ></View>
+          <View
+          style={{
+            backgroundColor: "yellow",
+            borderStyle: "solid",
+            borderWidth: 3,
+            width: "25%",
+            height: "16,5%"
+          }}
+          ></View>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          backgroundColor: "blue",
+          height: "16,5%"
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "red",
+            borderStyle: "solid",
+            borderWidth: 3,
+            width: "25%",
+            height: "16,5%"
+          }}
+          ></View>
+          <View
+          style={{
+            backgroundColor: "yellow",
+            borderStyle: "solid",
+            borderWidth: 3,
+            width: "25%",
+            height: "16,5%"
+          }}
+          ></View>
+                  <View
+          style={{
+            backgroundColor: "red",
+            borderStyle: "solid",
+            borderWidth: 3,
+            width: "25%",
+            height: "16,5%"
+          }}
+          ></View>
+          <View
+          style={{
+            backgroundColor: "yellow",
+            borderStyle: "solid",
+            borderWidth: 3,
+            width: "25%",
+            height: "16,5%"
+          }}
+          ></View>
+      </View>
+    </View>
   )
 }
 
