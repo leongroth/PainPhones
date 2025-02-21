69dcca2cbc8e673064f9eb9d05378b29b4cf7dcf
SnemandFrostHamSelv <mathias_frost_j@hotmail.com>
2025-02-21 12:16:49 +0100

    Update xFindIconTaskOne.tsx

--- app/xFindIconTaskOne.tsx
+++ app/xFindIconTaskOne.tsx
@@ -4,9 +4,37 @@ import Headline from '@/components/reusable/Headline'
 import Description from '@/components/reusable/Description';
 import PainButton from '@/components/reusable/PainButton';
 
+
 //Side 1 af FIND_ICON
 
 export default function xFindIconTaskOne() {
+
+  const images=[
+    'assets\Icons\amazon.png', 
+    'assets\Icons\Candy-Crush.jpg',
+    'assets\Icons\chatgpt.jpg',
+    'assets\Icons\ClashOfClans.jpg',
+    'assets\Icons\docs.png',
+    'assets\Icons\dropbox.svg',
+    'assets\Icons\facebook.webp', 
+    'assets\Icons\fruit-ninja.jpg',
+    'assets\Icons\Instagram_logo.webp',
+    'assets\Icons\linkedIn.webp',
+    'assets\Icons\messenger.png',
+    'assets\Icons\Netflix.jpg',
+    'assets\Icons\office365.png',
+    'assets\Icons\onenote.png',
+    'assets\Icons\pokemon-go.png',
+    'assets\Icons\reddit.png',
+    'assets\Icons\Snapchat.png',
+    'assets\Icons\soundcloud.jpg',
+    'assets\Icons\spotify.png',
+    'assets\Icons\strava.png',
+    'assets\Icons\Subway_surfers.jpg',
+    'assets\Icons\whatsapp.jpg',
+    'assets\Icons\Wish-emblem.png',
+    'assets\Icons\youtube.jpg'
+  ]
   return (
     <View
     style={{
