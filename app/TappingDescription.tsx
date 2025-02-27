a498538d32f07d118e9a504780b8658e005bbb6c
leongroth <leongroth@gmail.com>
2025-02-27 10:43:03 +0100

    push

--- app/TappingDescription.tsx
+++ app/TappingDescription.tsx
@@ -6,18 +6,22 @@ import PainButtonTwo from "../components/reusable/PainButtonTwo"
 import Target from "../components/reusable/Target"
 import { Button, View } from "react-native"
 
-const date = new Date()
-// `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`
-const [time, setTime] = useState([])
 
 
-const getTime = () => {
-  const newDate = new Date()
-  const newTime = `${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}.${newDate.getMilliseconds()}`
-  setTime((time) => [...time, newTime])
-}
 
 const TappingDescription = () => {
+
+  const date = new Date()
+  // `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`
+  const [time, setTime] = useState([`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`])
+
+
+  const getTime = () => {
+    const newDate = new Date()
+    const newTime = `${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}.${newDate.getMilliseconds()}`
+    setTime((time) => [...time, newTime])
+  }
+
   return (
     <View
         style={{
@@ -30,6 +34,9 @@ const TappingDescription = () => {
         <Description text={"A circle appears on the screen and you have to press it as quickly and accurately as possible"} />
         <PainButton href={"/TappingTasks"} text={"Continue"} />
         <PainButtonTwo text={"Get time"} onPress={getTime}/>
+        <View>{time.length}</View>
+
+
     </View>
   )
 }
