3b9db75c7e952ba16218f8de35eaec6b57c22196
leongroth <leongroth@gmail.com>
2025-02-27 10:34:09 +0100

    push

--- app/TappingDescription.tsx
+++ app/TappingDescription.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
 import Description from "../components/reusable/Description"
 import Headline from "../components/reusable/Headline"
 import PainButton from "../components/reusable/PainButton"
+import PainButtonTwo from "../components/reusable/PainButtonTwo"
 import Target from "../components/reusable/Target"
-import { View } from "react-native"
+import { Button, View } from "react-native"
+
+const date = new Date()
+// `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`
+const [time, setTime] = useState([])
+
+
+const getTime = () => {
+  const newDate = new Date()
+  const newTime = `${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}.${newDate.getMilliseconds()}`
+  setTime((time) => [...time, newTime])
+}
 
 const TappingDescription = () => {
   return (
@@ -16,6 +29,7 @@ const TappingDescription = () => {
         <Headline text={"Tapping-Task"}/>
         <Description text={"A circle appears on the screen and you have to press it as quickly and accurately as possible"} />
         <PainButton href={"/TappingTasks"} text={"Continue"} />
+        <PainButtonTwo text={"Get time"} onPress={getTime}/>
     </View>
   )
 }
